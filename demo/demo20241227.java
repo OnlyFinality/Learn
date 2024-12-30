4ec6f07eddc1d863433a7c94d96508cbad24e002
李星云 <3486159271@qq.com>
2024-12-30 09:00:39 +0800

    20241227

--- demo/demo20241227.java
+++ demo/demo20241227.java
@@ -3,9 +3,6 @@ package demo;
 public class demo20241227 {
     public static void main(String[] args) {
         System.out.println("Hello World");
-
-
-
-
+        System.out.println("12345");
     }
 }
