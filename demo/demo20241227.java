5f5c1acbe407826fbff34a82bbbabd1be50e188c
李星云 <3486159271@qq.com>
2024-12-30 09:01:08 +0800

    20241230

--- demo/demo20241227.java
+++ demo/demo20241227.java
@@ -4,5 +4,6 @@ public class demo20241227 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("12345");
+        System.out.println("98745");
     }
 }
