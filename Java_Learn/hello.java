eccc40d2bee53289feddaa6d65a0113aee69e31a
李星云 <3486159271@qq.com>
2024-12-30 17:06:14 +0800

    md

--- Java_Learn/hello.java
+++ Java_Learn/hello.java
@@ -3,5 +3,7 @@ package Java_Learn;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        System.out.println("123456789");
     }
 }
