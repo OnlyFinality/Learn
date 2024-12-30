b59491670b7626fe164a7a7fdca0b2e41fd96c32
李星云 <3486159271@qq.com>
2024-12-30 16:45:18 +0800

    允许局域网连接 MySQL

--- demo/demo20241227.java
+++
@@ -1,9 +0,0 @@
-package demo;
-
-public class demo20241227 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("12345");
-        System.out.println("98745");
-    }
-}
