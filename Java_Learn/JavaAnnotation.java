da7e714298b57d5de9db7f01cb504cb52c9e339e
李星云 <3486159271@qq.com>
2025-01-02 11:41:14 +0800

    update

---
+++ Java_Learn/JavaAnnotation.java
@@ -0,0 +1,20 @@
+package Java_Learn;
+
+public class JavaAnnotation {
+    public static void main(String[] args) {
+        // 这是一个单行注释
+        int x = 10; // 初始化一个变量x为10
+        /*
+        这是一个多行注释
+        可以用来注释多行代码
+        */
+        int y = 20; // 初始化一个变量y为20
+    }
+    /**
+     * 这是一个文档注释示例
+     * 它通常包含有关类、方法或字段的详细信息
+     */
+    public class MyClass {
+        // 类的成员和方法
+    }
+}
--- Java_Learn/hello.java
+++
@@ -1,8 +0,0 @@
-package Java_Learn;
-
-public class hello {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-
-    }
-}
